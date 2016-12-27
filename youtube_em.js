b01b6c940107f6aa5e789bbe3b7bf9345aebb00b
greengrams <j3242695@gmail.com>
2016-12-28 00:01:01 +0800

    ver1

---
+++ youtube_em.js
@@ -0,0 +1,41 @@
+function onYouTubeIframeAPIReady() {
+	var list = document.getElementsByClassName("youtube-audio");
+	var o = function (elmnt,i) {
+		var a = elmnt ? "bcrQbe8.png" : "pzAitH7.png";
+		list[i].t.setAttribute("src", "https://i.imgur.com/" + a);
+	};
+	for (var i = 0; i < list.length; i++) {
+		var e = list[i];
+		e.t = document.createElement("img");
+		e.t.setAttribute("id", "youtube-icon");
+		e.t.style.cssText = "cursor:pointer;cursor:hand";
+		e.t.ind=i;
+		e.appendChild(e.t);
+		var a = document.createElement("div");
+		a.setAttribute("id", "youtube-player"+i);
+		a.setAttribute("class", "youtube-player");
+		e.appendChild(a);
+		e.t.onclick = function (elmnt) {
+			elmnt.target.r.getPlayerState() === YT.PlayerState.PLAYING || elmnt.target.r.getPlayerState() === YT.PlayerState.BUFFERING ? (elmnt.target.r.pauseVideo(), o(!1,elmnt.target.ind)) : (elmnt.target.r.playVideo(), o(!0,elmnt.target.ind))
+		};
+		e.t.r = new YT.Player("youtube-player"+i, {
+			height : "0",
+			width : "0",
+			videoId : e.dataset.video,
+			playerVars : {
+				autoplay : e.dataset.autoplay,
+				loop : e.dataset.loop
+			},
+			events : {
+				onReady : function (elmnt) {
+					elmnt.target.setPlaybackQuality("small");
+					o(elmnt.target.getPlayerState() !== YT.PlayerState.CUED,elmnt.target.ind);
+				},
+				onStateChange : function (elmnt) {
+					elmnt.data === YT.PlayerState.ENDED && o(!1,elmnt.target.ind);
+				}
+			}
+		});
+		e.t.r.ind=i;
+	}
+}
